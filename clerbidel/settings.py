18ba1ccf74249579f6813cc0e1ce9b623c22a554
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-02-01 02:14:15 +0000

    turn off degug

--- clerbidel/settings.py
+++ clerbidel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,23 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3m+jg_%5u#eke$+(v(hd_f2_y4-btvjxt+sk6z0v^ih7fd2ovl'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+
+if DEBUG is False:
+    ALLOWED_HOSTS = [
+        'clerbidelestates',
+    ]
+
+if DEBUG is True:
+    ALLOWED_HOSTS = ['127.0.0.1','192.168.43.212 ']
+
+# ALLOWED_HOSTS = ['127.0.0.1','192.168.43.212 ','clerbidelestates']
 
 
 # Application definition
