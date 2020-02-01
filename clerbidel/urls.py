f6ab3e4d2da9bd2ee50dde2188e86f69dc02d319
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-02-01 03:32:25 +0000

    .

--- clerbidel/urls.py
+++ clerbidel/urls.py
@@ -25,12 +25,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/',include('estate_app.api_urls')),
 
-] 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = error_404
 handler500 = error_500
